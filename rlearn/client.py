a65a4dd2912fe1b80cbc99f3eedf57ac86ce85bc
JamaicanMoose <jackcollinsnj@gmail.com>
2019-02-11 10:06:44 -0500

    remote execution of keras models, test script
    - keras models now run on the server
    - in memory caching of data, models and jobs (shelve had errors when syncing)
    - test script (modified from Keras' MNIST CNN)

--- rlearn/client.py
+++ rlearn/client.py
@@ -7,89 +7,132 @@ import numpy as np
 import rlearn.serialization as rls
 import hashlib
 
+def digest(bytes):
+    return hashlib.md5(bytes).hexdigest()
 
-class Session:
+class RLearnSession:
     def __init__(self, serverUrl):
         self.server = 'ws://' + serverUrl
+        self.eventloop = asyncio.get_event_loop()
         self.session = None
+        self.connect()
 
-    # @classmethod
-    # async def connect(cls, serverUrl):
-    #     s = cls(serverUrl)
-    #     s.session = await websockets.connect(s.server)
-    #     return s
+    def __del__(self):
+        self.close()
+
+    #def train(self, type, model, mname, ):
+
+    def connect(self):
+        async def cnct():
+            self.session = await websockets.connect(self.server, max_size=None)
+        self.eventloop.run_until_complete(cnct())
+
+    def close(self):
+        async def cls():
+            await self.session.close()
+        self.eventloop.run_until_complete(cls())
 
     def addData(self, x, y, name):
         async def f():
             await self.send_data(x, y, name)
-        asyncio.get_event_loop().run_until_complete(f())
+        self.eventloop.run_until_complete(f())
+        
+    def addModel(self, model, name = ''):
+        async def f():
+            await self.send_model(model, name)
+        self.eventloop.run_until_complete(f())
 
-    async def train(self):
-        session = await websockets.connect(self.server)
+    def addJob(self, type, model, data, compileargs = {}, fitargs = {}):
+        async def f():
+            res = await self.send_job(type, model, data, compileargs, fitargs)
+            return rls.deserialize(res)
+        return self.eventloop.run_until_complete(f())
 
-
-    async def ping(self):
-        print('hi')
-        session = await websockets.connect(self.server)
-        await session.send(dill.dumps({'type': 'ping'}))
-        echo = await session.recv()
-        print(echo)
-        await session.close()
-
-    async def ping_context(self):
-        async with websockets.connect(self.server) as session:
-            await session.send(dill.dumps({'type': 'ping'}))
-            echo = await session.recv()
-            print(echo)
+    def listData(self):
+        async def f():
+            await self.list_data()
+        self.eventloop.run_until_complete(f())
 
     async def list_data(self):
-        session = await websockets.connect(self.server)
-        await session.send(dill.dumps({'type': 'listdata'}))
-        dlist = await session.recv()
-        print(dill.loads(dlist))
-        await session.close()
+        await self.session.send(dill.dumps({'type': 'listdata'}))
+        dlist = dill.loads(await self.session.recv())
+        print(dlist)
+        return dlist['list']
 
-    async def send_job(self, type, model, data, **kwargs):
-        session = await websockets.connect(self.server)
-        await session.send(dill.dumps({
+    async def list_models(self):
+        await self.session.send(dill.dumps({'type': 'listmodels'}))
+        mlist = dill.loads(await self.session.recv())
+        print(mlist)
+        return mlist['list']
+
+    async def send_job(self, type, model, data, compileargs, fitargs):
+        print('sending job')
+        await self.session.send(dill.dumps({
             'type': 'job',
             'jobinfo': {
-
+                'type': type,
+                'data': data,
+                'model': model,
+                'compileargs': compileargs,
+                'fitargs': fitargs
             }
         }))
+        res = dill.loads(await self.session.recv())
+        print(res['status'])
+        if res['status'] == 'SUCCESS':
+            return res['trained']
+        else:
+            raise Exception('Training Failed')
 
+    async def send_model(self, model, name):
+        onserv = await self.list_models()
+        mstr = rls.serialize(model).encode('utf-8')
+        hsh = digest(mstr)
+        nm = name if name != '' else hsh
+        modeldata = {
+            'name': nm,
+            'hash': hsh
+        }
+        samehsh = list(filter(lambda a: a[1] == hsh, onserv))
+        if samehsh:
+            samename = list(filter(lambda a: a[0] == nm, samehsh))
+            if samename:
+                return
+            else:
+                modeldata['copy'] = False
+                modeldata['copyname'] = samehsh[0][0]
+        else:
+            modeldata['copy'] = False
+            modeldata['modelstr'] = mstr
+        await self.session.send(dill.dumps({
+            'type': 'model',
+            'modeldata': modeldata
+        }))
+        resp = await self.session.recv()
+        print(dill.loads(resp))
 
     async def send_data(self, x, y, name):
-        session = await websockets.connect(self.server)
+        onserv = await self.list_data()
         dstr = dill.dumps({'x': rls.serialize(x), 'y': rls.serialize(y)})
-        await session.send(dill.dumps({
+        hsh = digest(dstr)
+        data = {'name': name, 'hash': hsh}
+        samehsh = list(filter(lambda a: a[1] == hsh, onserv))
+        if samehsh:
+            samename = list(filter(lambda a: a[0] == name, samehsh))
+            if samename:
+                return
+            else:
+                data['copy'] = True
+                data['copyname'] = samehsh[0][0]
+        else:
+            data['copy'] = False
+            data['datastr'] = dstr
+
+        await self.session.send(dill.dumps({
             'type': 'data',
-            'data': {
-                'name': name,
-                'hash': hashlib.md5(dstr).hexdigest(),
-                'datastr': dstr
-            }
+            'data': data
         }))
-        resp = await session.recv()
+
+        resp = await self.session.recv()
         print(dill.loads(resp))
-        await session.close()
-
-
-async def start():
-    s = Session('localhost:8765')
-    await s.send_data([1, 2, 3], [4, 5, 6], 'test')
-    await s.send_data([2, 2, 2], [3, 4, 5], 'test2')
-    await s.list_data()
-    print('done')
-
-#asyncio.get_event_loop().run_until_complete(start())
-
-s = Session('localhost:8765')
-s.addData(np.array([1, 2, 3]), np.array([4, 5, 6]), 'test')
-s.addData(np.array([2, 2, 2]), np.array([3, 4, 5]), 'test2')
-
-async def f2():
-    await s.list_data()
-
-asyncio.get_event_loop().run_until_complete(f2())
 
--- rlearn/server.py
+++ rlearn/server.py
@@ -10,28 +10,47 @@ import shelve
 
 # CLI Imports
 import argparse
+import signal
 
 # ML Imports
 import keras
 import numpy as np
 
-db = shelve.open('./shelf.db', writeback=True)
+#db = shelve.open('shelf', writeback=True)
+
+db = {}
+
+def keyboardInterruptHandler(sg, fr):
+    asyncio.get_event_loop().stop()
+    #db.close()
+    exit(0)
+
+
+signal.signal(signal.SIGINT, keyboardInterruptHandler)
 
 '''
 {
     kmodels: {
-        name: (hash, savedmodel)
+        name: {hash, modelstr}
     }
-    dataPairs: {
-        name: (hash, saveddata)
+    dataentries: {
+        name: {hash, saveddata}
+    }
+    jobs: {
+        'modelhash'+'datahash'+'argshash': {mname, dname, compileargs, fitargs, trainedmodel}
     }
 }
 '''
 
 if 'kmodels' not in db:
     db['kmodels'] = {}
-if 'dataPairs' not in db:
-    db['dataPairs'] = {}
+if 'dataentries' not in db:
+    db['dataentries'] = {}
+if 'jobs' not in db:
+    db['jobs'] = {}
+
+def digest(bytes):
+    return hashlib.md5(bytes).hexdigest()
 
 async def handle(websocket, path):
     async for message in websocket:
@@ -41,12 +60,16 @@ async def handle(websocket, path):
             await websocket.send('pong')
         elif obj['type'] == 'model':
             print('socket<model>')
+            await handle_model(websocket, obj['modeldata'])
         elif obj['type'] == 'data':
             print('socket<data>')
             await handle_data(websocket, obj['data'])
         elif obj['type'] == 'listdata':
             print('socket<listdata>')
             await handle_listdata(websocket)
+        elif obj['type'] == 'listmodels':
+            print('socket<listmodels>')
+            await handle_listmodels(websocket)
         elif obj['type'] == 'job':
             print('socket<job>')
             await handle_job(websocket, obj['jobinfo'])
@@ -60,18 +83,79 @@ async def handle_job(websocket, jobinfo):
         }
     '''
     if jobinfo['type'] == 'keras':
-        model = rls.deserialize(db['kmodels'][jobinfo['model']])
-        data = db['dataPairs'][jobinfo['data']]
-        x = rls.deserialize(data['x'])
-        y = rls.deserialize(data['y'])
-        model.compile(**(jobinfo['compileargs']))
-        model.fit(x=x, y=y, **(jobinfo['fitargs']))
+        #Get stored pairs
+        mdobj = db['kmodels'][jobinfo['model']]
+        dobj = db['dataentries'][jobinfo['data']]
+        #Create hash of args
+        argshsh = digest(dill.dumps(jobinfo['compileargs'])) + \
+                  digest(dill.dumps(jobinfo['fitargs']))
+        jobhash = mdobj['hash'] + dobj['hash'] + argshsh
+        #If weve already done this job, just return the result
+        if jobhash in db['jobs'] and db['jobs'][jobhash]['trainedmodel']:
+            await websocket.send(dill.dumps({
+                'status': 'SUCCESS',
+                'trained': db['jobs'][jobhash]['trainedmodel']
+            }))
+        else:
+            #Add job entry to db
+            db['jobs'][jobhash] = {
+                'mname': jobinfo['model'],
+                'dname': jobinfo['data'],
+                'compileargs': jobinfo['compileargs'],
+                'fitargs': jobinfo['fitargs'],
+                'trainedmodel': None
+            }
+            #Train model
+            model = rls.deserialize(str(mdobj['modelstr']))
+            x = rls.deserialize(dobj['dataobj']['x'])
+            y = rls.deserialize(dobj['dataobj']['y'])
+            model.compile(**(jobinfo['compileargs']))
+            model.fit(x=x, y=y, **(jobinfo['fitargs']))
+            mdstr = rls.serialize(model)
+            #Add trained model to job entry
+            db['jobs'][jobhash]['trainedmodel'] = mdstr
+            #db.sync()
+            #Send trained model
+            await websocket.send(dill.dumps({
+                'status': 'SUCCESS',
+                'trained': mdstr
+            }))
+
+async def handle_listmodels(websocket):
+    await websocket.send(dill.dumps({
+        'status': 'SUCCESS',
+        'list': [(k, db['kmodels'][k]['hash']) for k in db['kmodels'].keys()]
+    }))
 
 async def handle_listdata(websocket):
     await websocket.send(dill.dumps({
         'status': 'SUCCESS',
-        'list': list(db['dataPairs'].keys())
+        'list': [(k, db['dataentries'][k]['hash']) for k in db['dataentries'].keys()]
     }))
+
+async def handle_model(websocket, modeldata):
+    '''
+        {
+            'name': namestr
+            'hash': hash of modelstr
+            'modelstr': string saved model
+        }
+    '''
+    if modeldata['copy']:
+        print(modeldata['name'], ' in DB')
+        db['kmodels'][modeldata['name']] = db['dataentries'][modeldata['copyname']]
+    else:
+        db['kmodels'][modeldata['name']] = {
+            'hash': modeldata['hash'],
+            'modelstr': modeldata['modelstr']
+        }
+    #db.sync()
+    await websocket.send(dill.dumps({
+        'status': 'SUCCESS',
+        'hash': modeldata['hash']
+    }))
+
+
 
 async def handle_data(websocket, data):
     '''
@@ -82,19 +166,20 @@ async def handle_data(websocket, data):
         }
     '''
     dataobj = dill.loads(data['datastr'])
-    hash = hashlib.md5(data['datastr']).hexdigest()
-    if hash == data['hash']:
-        db['dataPairs'][data['name']] = (hash, dataobj)
-        await websocket.send(dill.dumps({
-            'status': 'SUCCESS',
-            'hash': hash
-        }))
+    if data['copy']:
+        print(data['name'], ' in DB')
+        db['dataentries'][data['name']] = db['dataentries'][data['copyname']]
     else:
-        await websocket.send(dill.dumps({
-            'status': 'FAILURE',
-            'reason': 'CHECKSUM MISMATCH'
-        }))
+        db['dataentries'][data['name']] = {
+            'hash': data['hash'],
+            'dataobj': dataobj
+        }
+    #db.sync()
+    await websocket.send(dill.dumps({
+        'status': 'SUCCESS',
+        'hash': data['hash']
+    }))
 
 asyncio.get_event_loop().run_until_complete(
-    websockets.serve(handle, 'localhost', 8765))
+    websockets.serve(handle, 'localhost', 8765, max_size=None))
 asyncio.get_event_loop().run_forever()
